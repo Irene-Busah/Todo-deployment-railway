79e9ce32cb568ac6d72b98933ac8feadd5cbf916
Irene-Busah <i.busah@alustudent.com>
2023-03-22 01:37:46 +0200

    new changes

--- todo/settings.py
+++ todo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yb_ir%5w7odhc$eu_yds*a7!5_282!hbs(y^nu3olz5y_$)e+z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
